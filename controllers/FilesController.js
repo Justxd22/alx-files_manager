863d981c430d63dcd4bfadc6bfb0ef0547528408
justxd22 <xdjust18@gmail.com>
2024-09-05 05:09:15 +0300

    get files

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -91,6 +91,73 @@ class FilesController {
 
     return res.status(201).send(newFile);
   }
+
+  static async getShow(req, res) {
+    const user = await userUtils.getUserFromReq(req);
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const id = user._id;
+
+    const fileId = req.params.id;
+
+    if (!valid.idOkay(fileId) || !valid.idOkay(id)) { return res.status(404).send({ error: 'Not found' }); }
+
+    const result = await dbClient.db.collection('files').findOne({
+      _id: ObjectId(fileId),
+      userId: ObjectId(id),
+    });
+
+    if (!result) return res.status(404).send({ error: 'Not found' });
+
+    const file = { id: result._id, ...result };
+    delete file.localPath;
+    delete file._id;
+
+    return res.status(200).send(file);
+  }
+
+  static async getIndex(req, res) {
+    const user = await userUtils.getUserFromReq(req);
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    let parentId = req.query.parentId || '0';
+    if (parentId === '0') parentId = 0;
+
+    let page = Number(req.query.page) || 0;
+
+    if (Number.isNaN(page)) page = 0;
+
+    if (parentId !== 0 && parentId !== '0') {
+      if (!valid.idOkay(parentId)) { return res.status(401).send({ error: 'Unauthorized' }); }
+
+      const folder = await dbClient.db.collection('files').findOne({
+        _id: ObjectId(parentId),
+      });
+
+      if (!folder || folder.type !== 'folder') { return res.status(200).send([]); }
+    }
+
+    const pipeline = [
+      { $match: { parentId } },
+      { $skip: page * 20 },
+      { $limit: 20 },
+    ];
+
+    const fileCursor = await dbClient.db.collection('files').aggregate(pipeline);
+
+    const fileList = [];
+    await fileCursor.forEach((doc) => {
+      const document = { id: doc._id, ...doc };
+      delete document.localPath;
+      delete document._id;
+      fileList.push(document);
+    });
+
+    return res.status(200).send(fileList);
+  }
 }
 
 export default FilesController;
